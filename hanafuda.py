3a14e31c3f08d8b789cc0fbbf94b53e3aa3b4e6e
平野聖剛 <seigo0301@icloud.com>
2022-11-12 19:21:24 +0900

    hanafuda

--- hanafuda.py
+++ hanafuda.py
@@ -476,7 +476,349 @@ class flower:
                 
                 
                 elif ((self.oya == 0) & (month % 2 == 1)) | ((self.oya == 1) & (month % 2 == 0)):  # 敵が親
-                    print('aiueo\n')
+                    # 敵のターン
+                    print("\n---- Enemy's turn : select -----\n")
+                    print("enemy's card: {}".format(self.your_cards))
+                    print("field cards: {}".format(self.field_cards))
+                    your_cards_month = []
+                    for your_card in self.your_cards:
+                        your_cards_month.append(your_card // 10)
+                    field_months = []
+                    for field_month in self.field_cards:
+                        field_months.append(field_month // 10)
+                    
+                    # 場のカードと月が一致するカードを手札の先頭から選択
+                    # ない場合は手札の先頭を出す
+                    select_card_enemy = 0
+                    for i in range(len(self.your_cards)):
+                        if your_cards_month[i] in field_months:
+                            select_card_enemy = self.your_cards[i]
+                    if select_card_enemy == 0:
+                        select_card_enemy = self.your_cards[0]
+                        
+                    select_month_enemy = select_card_enemy // 10
+                    print("enemy select:  {}".format(select_card_enemy))
+                    
+                                      
+                    # 選んだカードと同じ月のカードの枚数で処理を変える
+                    # 0枚の場合
+                    if field_months.count(select_month_enemy) == 0:
+                        self.your_cards.remove(select_card_enemy)
+                        self.field_cards.append(select_card_enemy)
+                    
+                    # 1枚の場合
+                    elif field_months.count(select_month_enemy) == 1:
+                        self.your_cards.remove(select_card_enemy)
+                        
+                        tmp_index = field_months.index(select_month_enemy)
+                        get_card_from_field = self.field_cards[tmp_index]
+                        self.field_cards.remove(get_card_from_field)
+                        
+                        self.your_getcard.append(select_card_enemy)
+                        self.your_getcard.append(get_card_from_field)
+                    
+                    # 3枚の場合
+                    elif field_months.count(select_month_enemy) == 3:
+                        self.your_cards.remove(select_card_enemy)
+                        
+                        get_cards_from_field = []
+                        for field_card in self.field_cards:
+                            if field_card // 10 == select_month_enemy:
+                                get_cards_from_field.append(field_card)
+                        
+                        self.your_getcard.append(select_card_enemy)
+                        for i in range(3):
+                            self.field_cards.remove(get_cards_from_field[i])
+                            self.your_getcard.append(get_cards_from_field[i])
+                    
+                    # 2枚の場合
+                    elif field_months.count(select_month_enemy) == 2:
+                        self.your_cards.remove(select_card_enemy)
+                        
+                        get_kouho_from_field = []
+                        for field_card in self.field_cards:
+                            if field_card // 10 == select_month_enemy:
+                                get_kouho_from_field.append(field_card)
+                        
+                        select_from_kouho = get_kouho_from_field[0]
+                        
+                        self.field_cards.remove(select_from_kouho)
+                        
+                        self.your_getcard.append(select_card_enemy)
+                        self.your_getcard.append(select_from_kouho)
+                    
+                    # その他の場合
+                    else:
+                        print("ERROR 3")
+                    
+                    print("Enemy's get cards:  {}".format(self.your_getcard))
+                    
+                    self.stage = draw_play_tefuda(self.stage, self.my_cards, self.my_getcard, self.your_cards, self.your_getcard, self.field_cards, displaymode)
+                    cv2.imshow("stage", self.stage)
+                    cv2.waitKey(0)
+                    #cv2.destroyAllWindows()
+                    
+                    
+                    
+                    
+                    # 山札から1枚引くするターン
+                    print("\n---- Enemy's turn : draw -----\n")
+                    print("Enemy's card: {}".format(self.your_cards))
+                    print("field cards: {}".format(self.field_cards))
+                    
+                    # yamafudaの先頭を取り出し，削除
+                    draw_card_enemy = self.yamafuda.pop(0)
+                    print("enemy draw:  {}".format(draw_card_enemy))
+                    
+                    # 描画
+                    self.stage = draw_play_yamafuda(self.stage, self.my_cards, self.my_getcard, self.your_cards, self.your_getcard, self.field_cards, draw_card_enemy, displaymode, timing=0)
+                    cv2.imshow("stage", self.stage)
+                    cv2.waitKey(0)
+                    #cv2.destroyAllWindows()
+
+                    draw_month_enemy = draw_card_enemy // 10
+                    field_months = []
+                    for i in range(len(self.field_cards)):
+                        field_months.append(self.field_cards[i] // 10)
+
+                                       
+                    # 山札から引いたカードと同じ月のカードが場に何枚あるかで処理を変える
+                    # 0枚の場合．引いたカードを場に置いて終了
+                    if field_months.count(draw_month_enemy) == 0:
+                        self.field_cards.append(draw_card_enemy)
+
+                    # 1枚の場合．山札から引いたカードと対応する場のカードをmy_getcardに追加
+                    elif field_months.count(draw_month_enemy) == 1:
+                        tmp_index = field_months.index(draw_month_enemy)
+                        get_card_from_field = self.field_cards[tmp_index]
+                        self.field_cards.remove(get_card_from_field)
+
+                        self.your_getcard.append(draw_card_enemy)
+                        self.your_getcard.append(get_card_from_field)
+
+                    # 3枚の場合．全取り
+                    elif field_months.count(draw_month_enemy) == 3:
+                        get_cards_from_field = []
+                        for field_card in self.field_cards:
+                            if field_card // 10 == draw_month_enemy:
+                                get_cards_from_field.append(field_card)
+                        
+                        self.your_getcard.append(draw_card_enemy)
+                        for i in range(3):
+                            self.field_cards.remove(get_cards_from_field[i])
+                            self.your_getcard.append(get_cards_from_field[i])
+                    
+                    # 2枚の場合．場のどのカードを取るかを選択させる
+                    elif field_months.count(draw_month_enemy) == 2:
+                        get_kouho_from_field = []
+                        for field_card in self.field_cards:
+                            if field_card // 10 == draw_month_enemy:
+                                get_kouho_from_field.append(field_card)
+
+                        select_from_kouho = get_kouho_from_field[0]
+                        self.field_cards.remove(select_from_kouho)
+
+                        self.your_getcard.append(draw_card_enemy)
+                        self.your_getcard.append(select_from_kouho)
+                    
+                    # その他の場合
+                    else:
+                        print("ERROR 2")
+                    
+                    
+                    print("Enemy's get cards:  {}".format(self.your_getcard))
+                    
+                    self.stage = draw_play_yamafuda(self.stage, self.my_cards, self.my_getcard, self.your_cards, self.your_getcard, self.field_cards, draw_card_enemy, displaymode, timing=1)
+                    cv2.imshow("stage", self.stage)
+                    cv2.waitKey(0)
+                    #cv2.destroyAllWindows()
+                    
+                    
+                    self.your_yaku, self.your_score = detect_yaku(self.your_getcard, self.your_score)
+                    
+                    print("Enemy's score: {}".format(self.your_score))
+
+
+
+
+
+
+
+                    # 手札から1枚選択するターン
+                    print("\n\n\n----- Your turn : select -----\n")
+                    print("your_cards: {}".format(self.my_cards))
+                    print("field_cards: {}".format(self.field_cards))
+                                        
+                    while True:
+                        select_key = int(input("select card    :"))
+                        if select_key in self.my_cards:
+                            break
+                        else:
+                            print("{} is not your cards.".format(select_key))
+
+                    
+                    select_month = select_key // 10
+                    field_months = []
+                    for i in range(len(self.field_cards)):
+                        field_months.append(self.field_cards[i] // 10)
+
+                                       
+                    # 選んだカードと同じ月のカードが場に何枚あるかで処理を変える
+                    # 0枚の場合．選んだカードを場に置く
+                    if field_months.count(select_month) == 0:
+                        self.my_cards.remove(select_key)
+                        self.field_cards.append(select_key)
+
+                    # 1枚の場合．選んだカードと対応する場のカードをmy_getcardに追加
+                    elif field_months.count(select_month) == 1:
+                        self.my_cards.remove(select_key)
+                        
+                        tmp_index = field_months.index(select_month)
+                        get_card_from_field = self.field_cards[tmp_index]
+                        self.field_cards.remove(get_card_from_field)
+
+                        self.my_getcard.append(select_key)
+                        self.my_getcard.append(get_card_from_field)
+
+                    # 3枚の場合．全取り
+                    elif field_months.count(select_month) == 3:
+                        self.my_cards.remove(select_key)
+
+                        get_cards_from_field = []
+                        for field_card in self.field_cards:
+                            if field_card // 10 == select_month:
+                                get_cards_from_field.append(field_card)
+                        
+                        self.my_getcard.append(select_key)
+                        for i in range(3):
+                            self.field_cards.remove(get_cards_from_field[i])
+                            self.my_getcard.append(get_cards_from_field[i])
+                    
+                    # 2枚の場合．場のどのカードを取るかを選択させる
+                    elif field_months.count(select_month) == 2:
+                        self.my_cards.remove(select_key)
+
+                        get_kouho_from_field = []
+                        for field_card in self.field_cards:
+                            if field_card // 10 == select_month:
+                                get_kouho_from_field.append(field_card)
+
+                        while True:
+                            select_from_kouho = int(input("type 1 card from {}    :".format(get_kouho_from_field)))
+                            if select_from_kouho in get_kouho_from_field:
+                                break
+                            else:
+                                print("you cannot select {}".format(select_from_kouho))
+                        self.field_cards.remove(select_from_kouho)
+
+                        self.my_getcard.append(select_key)
+                        self.my_getcard.append(select_from_kouho)
+                    
+                    # その他の場合
+                    else:
+                        print("ERROR 1")
+                    
+                    
+                    print("Your get cards:  {}".format(self.my_getcard))
+                    
+                    self.stage = draw_play_tefuda(self.stage, self.my_cards, self.my_getcard, self.your_cards, self.your_getcard, self.field_cards, displaymode)
+                    cv2.imshow("stage", self.stage)
+                    cv2.waitKey(0)
+                    #cv2.destroyAllWindows()
+                    
+                    
+                    
+                    # 山札から1枚引くするターン
+                    print("\n----- Your turn : draw -----\n")
+                    print("Your cards: {}".format(self.my_cards))
+                    print("field cards: {}".format(self.field_cards))
+                    
+                    # yamafudaの先頭を取り出し，削除
+                    draw_card = self.yamafuda.pop(0)
+                    print("you draw:  {}".format(draw_card))
+                    
+                    # 描画
+                    self.stage = draw_play_yamafuda(self.stage, self.my_cards, self.my_getcard, self.your_cards, self.your_getcard, self.field_cards, draw_card, displaymode, timing=0)
+                    cv2.imshow("stage", self.stage)
+                    cv2.waitKey(0)
+                    #cv2.destroyAllWindows()
+                    
+                    draw_month = draw_card // 10
+                    field_months = []
+                    for i in range(len(self.field_cards)):
+                        field_months.append(self.field_cards[i] // 10)
+
+                                       
+                    # 山札から引いたカードと同じ月のカードが場に何枚あるかで処理を変える
+                    # 0枚の場合．引いたカードを場に置いて終了
+                    if field_months.count(draw_month) == 0:
+                        self.field_cards.append(draw_card)
+
+                    # 1枚の場合．山札から引いたカードと対応する場のカードをmy_getcardに追加
+                    elif field_months.count(draw_month) == 1:
+                        tmp_index = field_months.index(draw_month)
+                        get_card_from_field = self.field_cards[tmp_index]
+                        self.field_cards.remove(get_card_from_field)
+
+                        self.my_getcard.append(draw_card)
+                        self.my_getcard.append(get_card_from_field)
+
+                    # 3枚の場合．全取り
+                    elif field_months.count(draw_month) == 3:
+                        get_cards_from_field = []
+                        for field_card in self.field_cards:
+                            if field_card // 10 == draw_month:
+                                get_cards_from_field.append(field_card)
+                        
+                        self.my_getcard.append(draw_card)
+                        for i in range(3):
+                            self.field_cards.remove(get_cards_from_field[i])
+                            self.my_getcard.append(get_cards_from_field[i])
+                    
+                    # 2枚の場合．場のどのカードを取るかを選択させる
+                    elif field_months.count(draw_month) == 2:
+                        get_kouho_from_field = []
+                        for field_card in self.field_cards:
+                            if field_card // 10 == draw_month:
+                                get_kouho_from_field.append(field_card)
+
+                        while True:
+                            select_from_kouho = int(input("type 1 card from {}    :".format(get_kouho_from_field)))
+                            if select_from_kouho in get_kouho_from_field:
+                                break
+                            else:
+                                print("you cannot select {}".format(select_from_kouho))
+                        self.field_cards.remove(select_from_kouho)
+
+                        self.my_getcard.append(draw_card)
+                        self.my_getcard.append(select_from_kouho)
+                    
+                    # その他の場合
+                    else:
+                        print("ERROR 2")
+                    
+                    
+                    print("Your get cards:  {}".format(self.my_getcard))
+                    
+                    # 描画
+                    self.stage = draw_play_yamafuda(self.stage, self.my_cards, self.my_getcard, self.your_cards, self.your_getcard, self.field_cards, draw_card, displaymode, timing=1)
+                    cv2.imshow("stage", self.stage)
+                    cv2.waitKey(0)
+                    #cv2.destoryAllWindows()
+                    
+                    
+                    
+                    
+                    # 獲得したカードをdetect_yakuに渡す．こいこい判定のため，前ターンの得点も渡す（初めて役がそろったときにこいこいするかを聞く）
+                    # 役が含まれるかの判定と，こいこいするかのinputはdetect_yakuで行う．
+                    # 返り値はリスト[[五光，四光，雨入り四光，三光，花見で一杯，月見で一杯，[猪鹿蝶，タネ枚数]，[赤短，タン枚数]，[青短，タン枚数]，[タネ，タネ枚数]，[タン，タン枚数]，[カス，カス枚数]], こいこい]
+                    # その役が成立していれば1，していなければ0が入る．
+                    # こいこいステータス:    0:こいこい前    1:こいこい後    2:こいこいしない or こいこい後役が揃う
+                    self.my_yaku, self.my_score = detect_yaku(self.my_getcard, self.my_score)
+                    
+                    print("your score: {}".format(self.my_score))
+
+
             
             
             print("\n\nend of month")
