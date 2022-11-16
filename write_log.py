06b79bb542bce7f392ae305b127d4e19078b516b
seigo-hirano <seigo0301@icloud.com>
2022-11-16 13:21:47 +0900

    hanafuda

---
+++ write_log.py
@@ -0,0 +1,6 @@
+#/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def write_init(file, num):
+    file.write('oya mode(0:player is oya of odd month, 1:player is oya of even month) = ' + str(num) + '\n')
+    return file
