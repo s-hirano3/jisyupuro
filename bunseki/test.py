f6df822dee4e1ea1a251c1ffc304c5df14675c56
平野聖剛 <seigo0301@icloud.com>
2022-12-13 16:32:11 +0900

    hanafuda

--- bunseki/test.py
+++
@@ -1,9 +0,0 @@
-import os
-
-path = "/Users/hiranoseigo/DOwnloads/log"
-filelist = os.listdir(path)
-for i in range(len(filelist)):
-    file = filelist[i]
-    if file[:12] == "log_computer":
-        print(file[12:])
-
